dace87c53bc7b85f5829c63f7582977fd3d3ba7c
xxuil <liuxxggwp@utexas.edu>
2018-02-11 15:43:56 -0600

    init

---
+++ src/ThreadSynch.java
@@ -0,0 +1,19 @@
+/*
+ * EID's of group members
+ * 
+ */
+import java.util.concurrent.Semaphore; // for implementation using Semaphores
+
+
+public class ThreadSynch {
+	
+	public ThreadSynch(int parties) {
+	}
+	
+	public int await() throws InterruptedException {
+           int index = 0;
+		
+          // you need to write this code
+	    return index;
+	}
+}
