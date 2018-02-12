301712523beda6d2b4c3519616fcb923c9126599
kyc375 <ckravis@utexas.edu>
2018-02-11 18:16:23 -0600

    ThreadSynch.java

--- src/ThreadSynch.java
+++ src/ThreadSynch.java
@@ -2,17 +2,27 @@
  * EID's of group members
  * 
  */
-import java.util.concurrent.Semaphore; // for implementation using Semaphores
+import java.util.concurrent.Semaphore;
 
 
 public class ThreadSynch {
-	
+	private Semaphore example;
+	private int parties;
+	private int count = 0;
 	public ThreadSynch(int parties) {
+		example = new Semaphore(0);
+		this.parties = parties;
 	}
 	
 	public int await() throws InterruptedException {
-           int index = 0;
-		
+		int index = parties - count;
+		if(example.getQueueLength() == parties - 1){
+			example.release(parties);
+			example = new Semaphore(0);
+		}else{
+			example.acquireUninterruptibly();
+		}
+		count++;
           // you need to write this code
 	    return index;
 	}
