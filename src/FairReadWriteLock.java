a91c689f69d7b2ac6128b37a46b83e65b602bc11
kyc375 <ckravis@utexas.edu>
2018-02-13 19:13:46 -0600

    FairReadWriteLock

--- src/FairReadWriteLock.java
+++ src/FairReadWriteLock.java
@@ -1,19 +1,23 @@
 import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class FairReadWriteLock {
 	boolean lockfor_reader = false;
-	int numWriter = 0;
-	final List<Timestamp> readstamp = new ArrayList<>();
-	final List<Timestamp> writestamp = new ArrayList<>();
-                        
+	final LinkedList<Timestamp> readstamp = new LinkedList<>();
+	final LinkedList<Timestamp> writestamp = new LinkedList<>();
+
+	FairReadWriteLock(){
+	}
 	public synchronized void beginRead() {
+		System.out.println(Thread.currentThread().getId() + " Reading");
 		Timestamp for_read = new Timestamp(System.currentTimeMillis());
 		readstamp.add(for_read);
 		while(!lockfor_reader || !writestamp.isEmpty() && for_read.after(writestamp.get(0))){			//A reader thread that invokes beginRead() will be blocked until all preceding writer threads have acquired and released the lock.
 			try {
+				System.out.println(Thread.currentThread().getId() + " Waiting to Read");
 				wait();
 			}catch(InterruptedException e){
 				e.printStackTrace();
@@ -22,16 +26,19 @@ public class FairReadWriteLock {
 	}
 
 	public synchronized void endRead() {
+		System.out.println(Thread.currentThread().getId() + " End Reading");
+		readstamp.removeFirst();
 		notifyAll();
 	}
 
 	public synchronized void beginWrite() {
+		System.out.println(Thread.currentThread().getId() + " Writing");
 		lockfor_reader = false;
-		numWriter++;
 		Timestamp for_write = new Timestamp(System.currentTimeMillis());
 		writestamp.add(for_write);
-		while(numWriter > 1 || lockfor_reader || !readstamp.isEmpty() && for_write.after(readstamp.get(0))){     //only one writer can do their stuff
+		while(lockfor_reader || !readstamp.isEmpty() && for_write.after(readstamp.get(0))){     //only one writer can do their stuff
 			try{
+				System.out.println(Thread.currentThread().getId() + " Waiting to Write");
 				wait();
 			}catch (InterruptedException e){
 				e.printStackTrace();
@@ -39,8 +46,9 @@ public class FairReadWriteLock {
 		}
 	}
 	public synchronized void endWrite() {
+		System.out.println(Thread.currentThread().getId() + " End Writing");
 		lockfor_reader = true;
-		numWriter--;
+		writestamp.removeFirst();
 		notifyAll();
 	}
 }
---
+++ src/FairReadWriteLockTester.java
@@ -0,0 +1,71 @@
+import java.util.concurrent.TimeUnit;
+
+public class FairReadWriteLockTester {
+
+    public static FairReadWriteLock lock = new FairReadWriteLock();
+    private static WriteThread[] writers;
+    private static ReadThread[] readers;
+    private static int attempts;
+
+    private static class ReadThread extends Thread {
+        public void run() {
+            for(int i = 0; i < attempts; i++){
+                lock.beginRead();
+                lock.endRead();
+                try {
+                    TimeUnit.MILLISECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static class WriteThread extends Thread {
+        public void run() {
+            for(int i = 0; i < attempts; i++){
+                    lock.beginWrite();
+                    lock.endWrite();
+                try {
+                    TimeUnit.MILLISECONDS.sleep(50);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void test(int numWriters, int numReaders, int numAttempts){
+
+        writers = new WriteThread[numWriters];
+        readers = new ReadThread[numReaders];
+        attempts = numAttempts;
+
+        for(int i = 0; i < writers.length; i++) {
+            writers[i] = new WriteThread();
+        }
+
+        for(int i = 0; i < readers.length; i++) {
+            readers[i] = new ReadThread();
+        }
+
+        int limit = writers.length;
+        if(readers.length > writers.length) {
+            limit = readers.length;
+        }
+
+        for(int i = 0; i < limit; i++) {
+            if(i < readers.length) {
+                readers[i].start();
+            }
+            if(i < writers.length) {
+                writers[i].start();
+            }
+        }
+    }
+    public static void main(String args[]){
+        test(5, 5, 5);
+    }
+}
