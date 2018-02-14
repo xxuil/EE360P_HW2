ca261c8ed2993619e2a56d149444ef857611ccfa
kyc375 <ckravis@utexas.edu>
2018-02-13 20:55:40 -0600

    FairReadWriteLock.java

--- src/FairReadWriteLock.java
+++ src/FairReadWriteLock.java
@@ -3,53 +3,80 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class FairReadWriteLock {
-	boolean lockfor_reader = false;
-	final LinkedList<Timestamp> readstamp = new LinkedList<>();
-	final LinkedList<Timestamp> writestamp = new LinkedList<>();
+	AtomicInteger count = new AtomicInteger(0);
+	int writerQ = 0;
+	int readerQ = 0;
+	final LinkedList<Integer> readstamp = new LinkedList<>();
+	final LinkedList<Integer> writestamp = new LinkedList<>();
 
-	FairReadWriteLock(){
-	}
 	public synchronized void beginRead() {
-		System.out.println(Thread.currentThread().getId() + " Reading");
-		Timestamp for_read = new Timestamp(System.currentTimeMillis());
-		readstamp.add(for_read);
-		while(!lockfor_reader || !writestamp.isEmpty() && for_read.after(writestamp.get(0))){			//A reader thread that invokes beginRead() will be blocked until all preceding writer threads have acquired and released the lock.
-			try {
-				System.out.println(Thread.currentThread().getId() + " Waiting to Read");
-				wait();
-			}catch(InterruptedException e){
-				e.printStackTrace();
-			}
-		}
-	}
-
-	public synchronized void endRead() {
-		System.out.println(Thread.currentThread().getId() + " End Reading");
-		readstamp.removeFirst();
-		notifyAll();
-	}
-
-	public synchronized void beginWrite() {
-		System.out.println(Thread.currentThread().getId() + " Writing");
-		lockfor_reader = false;
-		Timestamp for_write = new Timestamp(System.currentTimeMillis());
-		writestamp.add(for_write);
-		while(lockfor_reader || !readstamp.isEmpty() && for_write.after(readstamp.get(0))){     //only one writer can do their stuff
+		System.out.println(Thread.currentThread().getId() + " Attempt to Read");
+		int task = count.incrementAndGet();
+		readstamp.add(task);
+		while(writerQ > 0 || readerHasLess(task)){
 			try{
-				System.out.println(Thread.currentThread().getId() + " Waiting to Write");
+				System.out.println(Thread.currentThread().getId() + " Reader waiting");
 				wait();
 			}catch (InterruptedException e){
 				e.printStackTrace();
 			}
 		}
+		System.out.println(Thread.currentThread().getId() + " Start Reading");
+		readerQ++;
+		readstamp.remove(new Integer(task));
+	}
+
+	public synchronized void endRead() {
+		System.out.println(Thread.currentThread().getId() + " End Reading");
+		readerQ--;
+		notifyAll();
+	}
+
+	public synchronized void beginWrite() {
+		System.out.println(Thread.currentThread().getId() + " Attempt to Write");
+		int task = count.incrementAndGet();
+		writestamp.add(task);
+		while((writerQ > 0 || readerQ > 0) || (writerHasLess(task))){
+			try{
+				System.out.println(Thread.currentThread().getId() + " Writer waiting");
+				wait();
+			}catch (InterruptedException e){
+				e.printStackTrace();
+			}
+		}
+		System.out.println(Thread.currentThread().getId() + " Start Writing");
+		writerQ++;
+		writestamp.remove(new Integer(task));
 	}
 	public synchronized void endWrite() {
 		System.out.println(Thread.currentThread().getId() + " End Writing");
-		lockfor_reader = true;
-		writestamp.removeFirst();
+		writerQ--;
 		notifyAll();
+	}
+
+	private boolean readerHasLess(int taskNum){
+		for(Integer i : writestamp){
+			if(i < taskNum){
+				return true;
+			}
+		}
+		return false;
+	}
+	private boolean writerHasLess(int taskNum){
+		for(Integer i : writestamp){
+			if(i < taskNum){
+				return true;
+			}
+		}
+		for(Integer i : readstamp){
+			if(i < taskNum){
+				return true;
+			}
+		}
+		return false;
 	}
 }
 	
