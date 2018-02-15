44e7b2ce9ecda08ea4482c0cf24748a3dde67d7a
kyc375 <ckravis@utexas.edu>
2018-02-14 23:21:25 -0600

    MonitorThreadSynch.java

--- src/MonitorThreadSynch.java
+++ src/MonitorThreadSynch.java
@@ -6,34 +6,22 @@
 
 
 public class MonitorThreadSynch {
-	private int permits;
 	private int parties;
-	private int numofwait = 0;
+	private int count = 0;
 	public MonitorThreadSynch(int parties) {
-		permits = 0;
 		this.parties = parties;
 	}
 	
-	public int await() throws InterruptedException {
-		int index = 0;
-		if(numofwait == parties - 1){
-			release();
+	public synchronized int await() throws InterruptedException {
+		count++;
+		int index = parties - count;
+		if(index == 0){
+			count = 0;
+			notifyAll();
 		}else{
-			acquire();
+			wait();
 		}
           // you need to write this code
 	    return index;
-	}
-	public synchronized void acquire(){
-		numofwait++;
-		try{
-			wait();
-		}catch(InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-	public synchronized void release(){
-		numofwait = 0;
-		notifyAll();
 	}
 }
